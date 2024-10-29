3c9768f12de0b1a638a84b84179b1f9572860d14
Martin Svejda <svejdam13@gmail.com>
2024-10-29 16:58:09 +0100

    Add Main inputs

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
          * Conzole app for a rough calculation ot the prices and consumption of material for the construction of a stone wall
          */
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Stone Wall Calculator");
+        System.out.println("Foundation Calculator");
 
         System.out.print("Enter foundation width (m): ");
         double foundationWidth = scanner.nextDouble();
@@ -33,6 +33,7 @@ public class Main {
         System.out.printf("Total cost for the foundation is %.2f You will need to order %.2f m3 of concrete and %d m of steel bars.\n", totalCost, foundation.calculateConcreteVolume(), foundation.calculateRebarCount());
         System.out.println("Of course you need to add the price of the transport and digger works. ");
 
+        System.out.println("Stone Wall Calculator");
 
     }
 }
